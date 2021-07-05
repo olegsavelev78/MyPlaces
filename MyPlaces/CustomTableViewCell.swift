344dfa67583faef13be7b6fb87382c99e82a3b7b
Oleg Savelev <olegsavelev@list.ru>
2021-07-05 14:41:02 +0500

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Олег Савельев on 05.07.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
