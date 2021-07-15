7f7957966a5f9b70b8e00543bc9ded47083ab4ae
Oleg Savelev <olegsavelev@list.ru>
2021-07-16 00:07:05 +0500

    Add Location manager

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -65,15 +65,28 @@ class NewPlaceTableViewController: UITableViewController {
         }
     }
     
+    //MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let identifier = segue.identifier,
+              let mapVC = segue.destination as? MapViewController else {
+            return
+        }
+        mapVC.incomeSegueIdentifier = identifier
+        
+        if identifier == "showPlace" {
+            mapVC.place.name = placeName.text!
+            mapVC.place.location = placeLocation.text
+            mapVC.place.type = placeType.text
+            mapVC.place.imageData = placeImage.image?.pngData()
+            
+        }
+    }
+    
     func savePlace(){
         
-        var image: UIImage?
-        
-        if imageIsChanged {
-            image = placeImage.image
-        } else {
-            image = #imageLiteral(resourceName: "imagePlaceholder")
-        }
+        let image = imageIsChanged ? placeImage.image : #imageLiteral(resourceName: "imagePlaceholder")
         
         let imageData = image?.pngData()
       
